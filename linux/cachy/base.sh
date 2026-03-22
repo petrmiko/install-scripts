f2b276a16230b30f661e9c2d48596186c066de0e
Petr Miko <miko.petr@gmail.com>
2026-03-22 15:18:17 +0100

    [CachyOS] Missing bash directives, tailscale, ghostty and flatpak install

--- linux/cachy/base.sh
+++ linux/cachy/base.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+
+set -eu
+
 sudo pacman -S --noconfirm stow \
     bat htop eza zoxide teeldear \
     mise \
     nvtop s-tui \
     podman tig tmux \
     starship zsh-syntax-highlighting \
+    tailscale \
     ttf-jetbrains-mono-nerd ttf-firacode-nerd
--- linux/cachy/desktop.sh
+++ linux/cachy/desktop.sh
@@ -1,4 +1,14 @@
+#!/bin/bash
+
+set -eu
+
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+
 sudo pacman -S --noconfirm flatpak fuse2 \
+    ghostty \
     okular skanpage \
     vlc
-paru -S --noconfirm visual-studio-code-bin
+paru -S --noconfirm 1password visual-studio-code-bin
+
+# flatpaks
+sh $SCRIPT_DIR/../flatpak.sh --kde
